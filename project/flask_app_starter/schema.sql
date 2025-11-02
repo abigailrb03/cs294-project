30cfa5c34a6e30ad0f0b4a2ba878bf6d3e36e8ef
Rebecca Phuonghanh Dang <rdang@berkeley.edu>
2025-11-01 22:41:35 -0700

    Reorganize files into new directory structure
    - Delete requirements.txt since it'll be replaced with pyproject.toml and uv.lock file
    - Move README.md into src/project/README.md since it's the student spec
    - Rename META.md into README.md which is the staff README
    - Delete flask_app_starter
    - Rename spotify_webscrape to spotify-data

--- project/flask_app_starter/schema.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS songs;
-CREATE TABLE songs (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    song_link TEXT NOT NULL,
-    song_id TEXT NOT NULL,
-    track_name TEXT,
-    artist_name TEXT,
-    album_image_url TEXT
-);
